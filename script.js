b2c94357dec7db3c5cf1f55da55ed7dabd422847
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-04-02 19:19:26 -0300

    finalizado projeto
    entretanto falta refinamento

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let totalSliders = document.querySelectorAll('.slider--item').length;
 let currentSlide = 0;
 
+let sliderWidth = document.querySelector('.slider').clientWidth;
+
 document.querySelector('.slider--width').style.width =
-    `calc(100vw * ${totalSliders})`;
+    `${sliderWidth * totalSliders}px`;
+document.querySelector('.slider--controls').style.width =
+    `${sliderWidth}px`;
 document.querySelector('.slider--controls').style.height =
     `${document.querySelector('.slider').clientHeight}px`;
 
@@ -23,10 +27,62 @@ function goNext() {
     updateMargin();
 }
 
-function updateMargin() {
-    let newMargin = (currentSlide * document.body.clientWidth);
-    document.querySelector('.slider--width').style.marginLeft =
-        `-${newMargin}px`; 
-}
+window.onload = function () {
 
-setInterval(goNext, 2000);
+    let totalSliders = document.querySelectorAll('.slider--item').length;
+    let currentSlide = 0;
+
+    let slider = document.querySelector('.slider');
+    let sliderWidth = slider.clientWidth;
+
+    document.querySelector('.slider--width').style.width =
+        `${sliderWidth * totalSliders}px`;
+
+    document.querySelector('.slider--controls').style.width =
+        `${sliderWidth}px`;
+
+    document.querySelector('.slider--controls').style.height =
+        `${slider.clientHeight}px`;
+
+    function updateMargin() {
+        let newMargin = (currentSlide * sliderWidth);
+        document.querySelector('.slider--width').style.marginLeft =
+            `-${newMargin}px`;
+    }
+
+    function goPrev() {
+        currentSlide--;
+        if (currentSlide < 0) {
+            currentSlide = totalSliders - 1;
+        }
+        updateMargin();
+    }
+
+    function goNext() {
+        currentSlide++;
+        if (currentSlide > (totalSliders - 1)) {
+            currentSlide = 0;
+        }
+        updateMargin();
+    }
+
+    let timer = setInterval(goNext, 5000);
+
+    window.addEventListener('resize', () => {
+        sliderWidth = slider.clientWidth;
+
+        document.querySelector('.slider--width').style.width =
+            `${sliderWidth * totalSliders}px`;
+
+        document.querySelector('.slider--controls').style.width =
+            `${sliderWidth}px`;
+
+        document.querySelector('.slider--controls').style.height =
+            `${slider.clientHeight}px`;
+
+        updateMargin();
+    });
+
+};
+
+setInterval(goNext, 5000);
